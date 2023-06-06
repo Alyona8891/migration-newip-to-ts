d8215ea7d91a1d114cd7476236dcfc15aec66355
unknown <besssta888@mail.ru>
2023-06-07 01:34:07 +0300

    feat: add apiKey to project

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'e1bcde9bafd54548b8f71ea0dbeec84d', // получите свой ключ https://newsapi.org/
         });
     }
 }
