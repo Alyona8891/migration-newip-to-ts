282bc87756a7599503a08cf721bde6cba05e3d4f
unknown <besssta888@mail.ru>
2023-06-11 02:19:18 +0300

    feat: describe and export interfaces to project

---
+++ migration-newip-to-ts/src/types/index.ts
@@ -0,0 +1,14 @@
+export interface res {
+  status: string,
+  sources: string[],
+};
+
+export interface sources {
+  id: string,
+  name: string[],
+  description: string[],
+  url: string[],
+  category: string[],
+  language: string[],
+  country: string[],
+};
